4dfaa0287564d335fa1b2724232e1b844f7c4b7b
sixbenzene <1735995272@qq.com>
2024-12-13 10:28:32 +0800

    v1.0

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+# key与secret需要向b站申请开发者入住
 idCode="",  # 主播身份码
 appId=None,  # 应用id/
 key="",  # access_key
 secret="",  # access_key_secret
+
+# 发送弹幕功能需要用到一下参数
+csrf_token = ""
+cookie = ""
+roomid = "" # 房间id号
