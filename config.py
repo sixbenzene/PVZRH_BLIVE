aac754eb5fba84134bad2dcbf1fa9751bf044e3f
sixbenzene <1735995272@qq.com>
2024-12-13 18:04:48 +0800

    参数

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# key与secret需要向b站申请开发者入住
-idCode="",  # 主播身份码
-appId=None,  # 应用id/
-key="",  # access_key
-secret="",  # access_key_secret
+# key与secret需要向b站申请开发者入驻，appId也是申请后才有
+idCode=""  # 主播身份码
+appId=  # 应用id/
+key=""  # access_key
+secret=""  # access_key_secret
 
 # 发送弹幕功能需要用到一下参数
 csrf_token = ""
