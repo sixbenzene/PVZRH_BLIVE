99c94d6b0a67436a32bc120d1e273df892b6120a
sixbenzene <sixbenzene@gmail.com>
2025-07-14 10:20:19 +0800

    paramter

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-idCode = ""
-appId=
-key=""
-secret=""
+idCode = "" #开发者平台获取
+appId=  #开发者平台获取
+key="" # B站开发者入住获取
+secret="" # B站开发者入住获取
 host="https://live-open.biliapi.com"
-room_id = 
-ruid = 
+room_id = # 直播间房间号
+ruid =   # 网页主页个人资料有一个uid
 
 csrf_token = ''
 cookie = ""
